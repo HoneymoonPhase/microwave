e43d9126b509a342a35130c944c08dfc56417686
Massimiliano Bisceglie <maxbisceglie89@gmail.com>
2020-11-14 12:58:25 +0000

    images added

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Menu from '../../assests/icons/setup.png';
+import Menu from '../../assests/icons/dropdown.png';
 import './style.css';
 
 const Navbar = (props) => {
   return(
     <div className='navbar'>
-      <img src={Menu} alt='dropdown' width='30' />
+      <img src={Menu} alt='dropdown' width='35' />
     </div>
   )
 }
