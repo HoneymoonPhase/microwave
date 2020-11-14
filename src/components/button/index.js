61909bf204587d2069a4d68a680c562f899442ea
Massimiliano Bisceglie <maxbisceglie89@gmail.com>
2020-11-14 15:40:27 +0000

    banner done

---
+++ src/components/button/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+
+const Button = () => (
+      <div>
+        <div className='button'>
+          <p>DISCOVER</p>
+        </div>
+      </div>
+  );
+
+export default Button;
--- src/components/container/index.js
+++ src/components/container/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import Navbar from '../navbar';
+import Banner from '../banner';
+import Button from '../button';
 import './style.css';
 
 const Container = (props) => {
   return(
     <div className='container'>
       <Navbar />
+      <div className='wrapper'>
+        <Banner />
+        <Button />
+      </div>
     </div>
   )
 }
