21089f93ab1dc07fda237bfd01eaf60716dca92d
Massimiliano Bisceglie <maxbisceglie89@gmail.com>
2020-11-14 15:57:16 +0000

    button styled

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -2,11 +2,9 @@ import React from 'react';
 import './style.css';
 
 const Button = () => (
-      <div>
-        <div className='button'>
-          <p>DISCOVER</p>
-        </div>
-      </div>
+    <div className='button'>
+      <p>DISCOVER</p>
+    </div>
   );
 
 export default Button;
