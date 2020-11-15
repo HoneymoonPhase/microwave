77310c0360b0f2ba465e1305fddfcb2d73bd8124
Massimiliano Bisceglie <maxbisceglie89@gmail.com>
2020-11-15 02:00:20 +0000

    button done

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import './style.css';
 
-const Button = () => (
-    <div className='button'>
+const Button = () => {
+
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext('2d');
+
+    ctx.beginPath();
+    ctx.fillStyle = '#dedede';
+    ctx.moveTo(13, 23);
+    ctx.lineTo(176, 23);
+    ctx.lineTo(187, 34);
+    ctx.lineTo(187, 77);
+    ctx.lineTo(24, 77);
+    ctx.lineTo(13, 66);
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.fillStyle = '#9B3E37';
+    ctx.moveTo(15, 25);
+    ctx.lineTo(175, 25);
+    ctx.lineTo(185, 35);
+    ctx.lineTo(185, 75);
+    ctx.lineTo(25, 75);
+    ctx.lineTo(15, 65);
+    ctx.fill();
+  });
+    
+
+  return(
+    <div className='wrapper-canvas'>
+        <canvas ref={canvasRef} width='200' height='100' className='canvas'/>
+      <div className='button'>
       <p>DISCOVER</p>
+      </div>
     </div>
   );
-
+}
+  
 export default Button;
---
+++ src/components/social/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Social = (props) => (
+      <div>
+        <img
+      </div>
+  );
+
+export default Social;
