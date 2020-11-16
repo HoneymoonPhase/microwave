daf70f49d5776d55a9bccc4eefd2ac2b6d1bd825
Massimiliano Bisceglie <maxbisceglie89@gmail.com>
2020-11-16 14:39:27 +0000

    video added

--- src/components/bandRoute/index.js
+++ src/components/bandRoute/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Band from '../band';
 import { Route } from 'react-router-dom';
-// import Mr_Ickyman from '../../assets/video.mp4';
+import Mr_Ickyman from '../../assets/Mr_Ickyman.mp4';
 import './style.css';
 
 const BandRoute = () => {
   return(
     <div>
-      <Route path='/band/honeymoonphase' render={(props) => <Band {...props} name={`HONEYMOON PHASE`} title={'Mr Ickiman'}/>} />
+      <Route path='/band/honeymoonphase' render={(props) => <Band {...props} name={`HONEYMOON PHASE`} title={'Mr Ickiman'} videoSrc={Mr_Ickyman} />} />
     </div>
   )
 }
