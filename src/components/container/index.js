0dc51fd3a8effcdd763e70cd69f838998758e23f
Massimiliano Bisceglie <maxbisceglie89@gmail.com>
2020-11-16 17:12:11 +0000

    changed paths

--- src/components/container/index.js
+++ src/components/container/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import Navbar from '../navbar';
 import Wrapper from '../wrapper';
-import BandRoute from '../bandRoute';
+import Band from '../band';
+import Mr_Ickyman from '../../assets/Mr_Ickyman.mp4';
+// import BandRoute from '../bandRoute';
 import { Route } from 'react-router-dom';
 import './style.css';
 
@@ -10,7 +12,8 @@ const Container = () => {
     <div className='container'>
       <Navbar />
       <Route exact path='/microwave/' component={Wrapper} />
-      <BandRoute />
+      <Route exact path='/microwave/band/honeymoonphase' render={(props) => <Band {...props} name={`HONEYMOON PHASE`} title={'Mr. Ickiman'} videoSrc={Mr_Ickyman} />} />
+      {/* <BandRoute /> */}
     </div>
   )
 }
