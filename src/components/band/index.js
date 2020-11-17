ad6756556e4baae38a81535a079878c2e18f82dd
Massimiliano Bisceglie <maxbisceglie89@gmail.com>
2020-11-17 02:38:24 +0000

    added spotify, youtube and apple for social

--- src/components/band/index.js
+++ src/components/band/index.js
@@ -2,16 +2,24 @@ import React from 'react';
 import Name from '../name';
 import Title from '../title';
 import Video from '../video';
+import Social from '../social';
 import './style.css';
 
 const Band = (props) => {
 
-  const { name, title, videoSrc } = props;
+  const { name, title, videoSrc, social } = props;
   return(
     <div className='band-container'>
       <Name name={name} />
       <Video videoSrc={videoSrc} />
       <Title title={title} />
+      <Social
+      facebook_link={social.facebook}
+      instagram_link={social.instagram}
+      spotify_link={social.spotify}
+      youtube_link={social.youtube}
+      apple_link={social.apple}
+      />
     </div>
   )
 }
--- src/components/social/index.js
+++ src/components/social/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import facebook from '../../assets/icons/facebook.png';
 import instagram from '../../assets/icons/instagram.png';
+import spotify from '../../assets/icons/spotify.png';
+import youtube from '../../assets/icons/youtube.png';
+import apple from '../../assets/icons/apple.png';
 import './style.css';
 
 const Social = (props) => {
 
-  const {instagram_link, facebook_link} = props;
+  const {
+    instagram_link,
+    facebook_link,
+    spotify_link,
+    youtube_link,
+    apple_link
+  } = props;
   
   const hasSocialLink = (socialLink, icon, social) => {
-    if (social) {
+    if (socialLink) {
       return(
         <a href={socialLink} target='_blank' rel='noreferrer'>
-          <img src={icon} alt={social} width='40' />
+          <img src={icon} alt={social} width='35' />
         </a>
       )
     } else {
@@ -23,6 +32,9 @@ const Social = (props) => {
     <div className='wrapper-social'>
       {hasSocialLink(facebook_link, facebook, 'facebook')}
       {hasSocialLink(instagram_link, instagram, 'instagram')}
+      {hasSocialLink(spotify_link, spotify, 'spotify')}
+      {hasSocialLink(youtube_link, youtube, 'youtube')}
+      {hasSocialLink(apple_link, apple, 'apple')}
     </div>
   )
 }
