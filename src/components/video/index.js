eb8cdd4a8e0cd9bc37f4f99233b4bbdec502b456
Massimiliano Bisceglie <maxbisceglie89@gmail.com>
2020-11-17 01:13:15 +0000

    embedded youtube video

--- src/components/video/index.js
+++ src/components/video/index.js
@@ -3,10 +3,15 @@ import './style.css';
 
 const Video = (props) => {
 
-  const { videoSrc } = props;
+  // const { videoSrc } = props;
   return(
     <div>
-      <video controls src={videoSrc} type='video/mp4' className='video-element'></video>
+      {/* <video controls src={videoSrc} type='video/mp4' className='video-element'></video> */}
+      <iframe className='video-element' title='honeymoon'
+      src="https://www.youtube.com/embed/ipT9OIGwoF4"
+      frameBorder="0" allow="accelerometer; autoplay;
+      clipboard-write; encrypted-media; gyroscope;
+      picture-in-picture" allowFullScreen></iframe>
     </div>
   )
 }
